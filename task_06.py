35b241a70d2d71b317bbc536892de8e7b90145d6
esipovdenis <denismart1303@gmail.com>
2026-02-06 09:53:34 +0500

    Выполнил задание

--- task_06.py
+++ task_06.py
@@ -2,8 +2,8 @@ r = int(input())
 g = int(input())
 b = int(input())
 color = (r, g, b)
-grey = (color[0], color[1], color[2])
-if r == g == b:
+# grey = (color[0], color[1], color[2])
+if color[0] == color[1] == color[2]:
     print("Да")
 else:
     print("Нет")
